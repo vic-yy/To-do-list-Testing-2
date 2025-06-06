6b6892750029aafb4717c9ef1889ed44f3c0486f
VKenji-pa <84997899+VKenji-pa@users.noreply.github.com>
2025-06-05 23:12:08 -0300

    feat: 4 testes MemoForm and test setup fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: 'jsdom',
+    // Aponta para o seu arquivo de setup
+    setupFiles: './__tests__/setupTests.js',
   },
-})
+});
